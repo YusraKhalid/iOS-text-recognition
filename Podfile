6efaa36867f31d2f246818185987c184dfc99400
Yusra Khalid <yusra.khalid@outlook.com>
2018-03-17 22:43:00 +0500

    UI implimented, including camera

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ios-text-recognition' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ios-text-recognition
+  pod 'TesseractOCRiOS', '4.0.0'
+
+  target 'ios-text-recognitionTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ios-text-recognitionUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ios-text-recognition/ViewController.swift
+++ ios-text-recognition/ViewController.swift
@@ -7,9 +7,62 @@
 //
 
 import UIKit
+//import TesseractOCR
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var rateOutput: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func captureImage(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction ) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            else{
+                let action2 = UIAlertController(title: "Error", message: "Camera Not available.", preferredStyle: .alert)
+                action2.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+                self.present(action2, animated: true, completion: nil)
+            }
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .default, handler: {(action:UIAlertAction ) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true,completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    
+    }
+    
+    @IBAction func pushImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func pushImageAndData(_ sender: UIButton) {
+    }
+    
+    @IBAction func uploadImage(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
