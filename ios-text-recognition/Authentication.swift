98f18957867670eace20dd221028d74626eaf8c6
Yusra Khalid <yusra.khalid@outlook.com>
2018-03-31 19:44:22 +0500

    Login interface added, mid commit

---
+++ ios-text-recognition/Authentication.swift
@@ -0,0 +1,15 @@
+//
+//  Authentication.swift
+//  ios-text-recognition
+//
+//  Created by Yusra Khalid on 3/31/18.
+//  Copyright © 2018 Yusra Khalid. All rights reserved.
+//
+
+import Foundation
+
+class Authentication{
+    
+    
+    
+}
